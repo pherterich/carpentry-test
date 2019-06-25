80b138e3da4cba9e78e59d427428ad495f0711a8
Patricia Herterich <suomenporo@gmail.com>
2019-06-25 15:19:49 +0100

    Update swc-installation-test-2.py

--- setup/swc-installation-test-2.py
+++ setup/swc-installation-test-2.py
@@ -92,29 +92,29 @@ CHECKS = [
     'git',
     'hg',              # Command line tool
     #'mercurial',       # Python package
-    'EasyMercurial',
+ #   'EasyMercurial',
 # Build tools and packaging
-    'make',
-    'virtual-pypi-installer',
-    'setuptools',
+ #   'make',
+  #  'virtual-pypi-installer',
+   # 'setuptools',
     #'xcode',
 # Testing
-    'nosetests',       # Command line tool
-    'nose',            # Python package
-    'py.test',         # Command line tool
-    'pytest',          # Python package
+    #'nosetests',       # Command line tool
+    #'nose',            # Python package
+    #'py.test',         # Command line tool
+    #'pytest',          # Python package
 # SQL
     'sqlite3',         # Command line tool
     'sqlite3-python',  # Python package
 # Python
-    'python',
-    'ipython',         # Command line tool
-    'jupyter',         # Former IPython features, notebook, etc.
-    'argparse',        # Useful for utility scripts
-    'numpy',
-    'scipy',
-    'matplotlib',
-    'pandas',
+   # 'python',
+   # 'ipython',         # Command line tool
+   # 'jupyter',         # Former IPython features, notebook, etc.
+   # 'argparse',        # Useful for utility scripts
+   # 'numpy',
+   # 'scipy',
+   # 'matplotlib',
+   # 'pandas',
     #'sympy',
     #'Cython',
     #'networkx',
